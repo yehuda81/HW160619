e63a6173aaa3f8a67c4bf7400983aca7690a2edc
yehuda81 <44754603+yehuda81@users.noreply.github.com>
2019-06-18 13:13:56 +0300

    שאלה 10

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static ManualResetEvent Gate = new ManualResetEvent(false);
+        static void Main(string[] args)
+        {
+           
+            //Task t = new Task(ConnectToDB);            
+            //t.Start();
+            //t.Wait();
+            new Thread(() => ConnectToDB()).Start();
+            new Thread(() => CreateCustomer()).Start();
+            new Thread(() => DelateOrder()).Start();            
+            
+
+           
+        }
+
+        private static void ConnectToDB()
+        {
+            Thread.Sleep(4000);
+            Console.WriteLine("Conecting to Date Base");
+            Gate.Set();
+        }
+
+        private static void DelateOrder()
+        {
+            Gate.WaitOne();
+            Console.WriteLine("Order has been delated!");
+        }
+
+        private static void CreateCustomer()
+        {
+            Gate.WaitOne();
+            Console.WriteLine("Customer created!");
+        }
+    }
+}
