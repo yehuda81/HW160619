c4f528131e5911882f196303a4d47f5b8d2c8dd5
yehuda81 <44754603+yehuda81@users.noreply.github.com>
2019-06-18 13:43:07 +0300

    שאלה 11

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            Task<int> t = new Task<int>(() => new Random().Next(1000,2000));
+            t.Start();
+            Thread.Sleep(t.Result);            
+            Task<int> t2 = new Task<int>(() => new Random().Next(1000, 2000));
+            t2.Start();
+            Thread.Sleep(t2.Result);
+            Task.WaitAny(t, t2);
+            Console.WriteLine(t.Result);
+            Console.WriteLine(t2.Result);
+        }
+    }
+}
